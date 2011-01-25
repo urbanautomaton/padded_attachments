a047d69005c4de75eb579993af98d0cc0f4e3796
Simon Coffey <simon@tribesports.com>
2011-01-25 16:12:58 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ThumbnailTest::Application.config.secret_token = '8a83c826598ca574851a7a063b2fda9772210992e2fb90b12374a89e431d7572f90c1c3dafddd4b4f97d47376a8414d1aaf7781d3f3419f14198c94d881ed417'
